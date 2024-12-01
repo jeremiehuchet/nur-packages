5226c1291149ac6d803a92fcaa7b8824044b96f5
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2024-12-01 14:26:01 +0100

    ♻️ Switch from owenthereal/ccat to batmac/ccat

--- pkgs/ccat/default.nix
+++ pkgs/ccat/default.nix
@@ -1,16 +1,44 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ stdenvNoCC, lib, fetchurl, unzip }:
 
-let github = lib.importJSON ./github.json;
-in buildGoPackage {
+let
+  github = lib.importJSON ./github-release-assets.json;
+  version = lib.strings.removePrefix "v" github.version;
+in stdenvNoCC.mkDerivation {
   pname = "ccat";
-  version = github.version;
-  goPackagePath = "github.com/jingweno/ccat";
-  src = fetchFromGitHub { inherit (github) owner repo rev hash; };
+  inherit version;
+
+  src = let
+    platformName = {
+      x86_64-linux = "linux-amd64";
+      x86_64-darwin = "darwin-amd64";
+    }.${stdenvNoCC.hostPlatform.system} or (throw "unsupported system ${stdenvNoCC.hostPlatform.system}");
+    checksums = fetchurl {
+      name = "checksums.txt";
+      url = github.assets."checksums.txt".url;
+      hash = github.assets."checksums.txt".hash;
+    };
+  in fetchurl {
+    url = github.assets."ccat-${version}-${platformName}.tar.gz".url;
+    hash = github.assets."ccat-${version}-${platformName}.tar.gz".hash;
+    downloadToTemp = true;
+    postFetch = ''
+      cp $downloadedFile $name
+      cat ${checksums} | grep $name | sha256sum --check
+      cp $name $out
+    '';
+  };
+
+  unpackPhase = ''
+    tar -zxf $src
+  '';
+
+  installPhase = ''
+    install -Dm755 ccat "$out/bin/ccat"
+  '';
 
   meta = with lib; {
-    description =
-      "ccat is the colorizing cat. It works similar to cat but displays content with syntax highlighting.";
+    description = "cat on steroids";
     license = licenses.mit;
-    homepage = "https://github.com/owenthereal/ccat";
+    homepage = "https://github.com/batmac/ccat";
   };
 }
